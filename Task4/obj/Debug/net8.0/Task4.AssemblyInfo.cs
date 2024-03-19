8b697c6747349471f5481529d176690107608a22
IlyaPetrovich <ilyamonya@gmail.com>
2024-03-19 20:49:21 +0900

    Task4

--- Task4/obj/Debug/net8.0/Task4.AssemblyInfo.cs
+++ Task4/obj/Debug/net8.0/Task4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cb27a1efb87cb56878f8f2229e23d05d94de429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db091158731ed3faf84959634d6347a63cfdadd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
