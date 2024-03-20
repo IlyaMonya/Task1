ac4ebe9de37b72b29d751f34c8a402a0cff9ef0a
IlyaPetrovich <ilyamonya@gmail.com>
2024-03-20 11:59:59 +0900

    add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 2, 4, 5, 6, 7, 84, 2, 5, 6, 1 };
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i++;
+}
